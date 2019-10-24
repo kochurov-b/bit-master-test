7cb7d71248c98d59cafd9571f948396f420a55d0
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-24 12:53:49 +0400

    create app structure

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import YMaps from "../YMaps";
 
 import "./styles.css";
 
 const App: React.FC = () => {
-  return <div className="App">hello</div>;
+  return (
+    <div className="app">
+      <div className="app__header"></div>
+      <div className="app__content">
+        <div className="app_ymaps">
+          <YMaps />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
