e3a3fc138e516a4c68337b22b55b15c736b7a67f
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-24 12:50:29 +0400

    create get address type

--- src/types/YMaps.ts
+++ src/types/YMaps.ts
@@ -1,2 +1,20 @@
 export type CoordsType = [number, number];
 export type GetCoordsType = { get: (arg0: string) => CoordsType };
+export type GetAddressType = {
+  geocode: (
+    arg0: number[]
+  ) => {
+    then: (
+      arg0: (result: {
+        geoObjects: {
+          get: (
+            arg0: number
+          ) => {
+            getThoroughfare: () => string | undefined;
+            getPremiseNumber: () => string | undefined;
+          };
+        };
+      }) => void
+    ) => void;
+  };
+};
