28bf8786c0b78fe855ac02d394e5d93a6f44993e
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-25 01:59:44 +0400

    create types for ymaps

---
+++ src/types/ymaps.ts
@@ -0,0 +1,20 @@
+export type CoordsType = [number, number];
+export type GetCoordsType = { get: (arg0: string) => CoordsType };
+export type GetAddressType = {
+  geocode: (
+    arg0: number[]
+  ) => {
+    then: (
+      arg0: (result: {
+        geoObjects: {
+          get: (
+            arg0: number
+          ) => {
+            getThoroughfare: () => string;
+            getPremiseNumber: () => string;
+          };
+        };
+      }) => void
+    ) => void;
+  };
+};
