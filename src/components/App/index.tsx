eefed992f96809c1c749b38b35c15d14ac19dec2
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-25 15:14:04 +0400

    added autocomplete to app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import YMaps from "../YMaps";
 import Crews from "../Crews";
+import Autocomplete from "../Autocomplete";
 
 import "./styles.css";
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <div className="app__header"></div>
+      <div className="app__header">
+        <Autocomplete />
+      </div>
       <div className="app__content">
         <div className="app__ymaps">
           <YMaps />
