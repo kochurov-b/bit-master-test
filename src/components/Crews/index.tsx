a2df390ba85ed5f2343c823d64f973cb0cf78799
Bogdan Kochurov <b02k5@b02k5.local>
2019-10-27 14:13:58 +0400

    get not_found from location state to Crews

--- src/components/Crews/index.tsx
+++ src/components/Crews/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { StoreType } from "../../store";
-import { ICrew } from "../../types/store/crews";
+import { ICrew, ICrewsState } from "../../types/store/crews";
 import Crew from "./Crew";
 import { selectCrew } from "../../store/actions/crews";
 
@@ -10,24 +10,21 @@ import "./styles.css";
 
 export default () => {
   const crews = useSelector<StoreType, Array<ICrew>>(state => state.crews.data);
-  const locationNotFound = useSelector<StoreType, boolean>(
-    state => state.location.notFound
-  );
-  const selectedCrew = useSelector<StoreType, number | null>(
-    state => state.crews.select_crew
+  const { select_crew, not_found } = useSelector<StoreType, ICrewsState>(
+    state => state.crews
   );
   const dispatch = useDispatch();
 
   return (
     <div className="crews">
-      {crews.length !== 0 && !locationNotFound ? (
+      {crews.length !== 0 && !not_found ? (
         <ul className="crews__list">
           {crews.map((crew, index) => {
             const { crew_id, car_mark, car_model, car_color, distance } = crew;
             return (
               <li key={crew_id.toString()} className="crews__item">
                 <Crew
-                  className={selectedCrew === crew_id ? "_selected-crew" : ""}
+                  className={select_crew === crew_id ? "_selected-crew" : ""}
                   mark={car_mark}
                   model={car_model}
                   color={car_color}
@@ -39,7 +36,7 @@ export default () => {
             );
           })}
         </ul>
-      ) : crews.length === 0 && !locationNotFound ? (
+      ) : crews.length === 0 && !not_found ? (
         <span className="crews__prompt">
           Выберите место на карте или заполните поле "Откуда", чтобы увидеть
           подходящие автомобили!
